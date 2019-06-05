a9f7199648cfd1a0659f781040642820b961a7ad
esl405 <50837406+esl405@users.noreply.github.com>
2019-06-05 19:18:24 -0400

    updated response

--- game.py
+++ game.py
@@ -1,6 +1,14 @@
 print("Welcome to R-P-S-InClass")
 
-user_choice=input("Select: Rock, Paper, Scissors, Fuck It: ")
+user_choice=input("Select: Rock, Paper, Scissors: ")
 
-print("")
-print("Selected:", user_choice)
+#if user_choice in ["rock", "paper","scissors"]:
+#print ("Selection:")
+#else:
+#    print ("fuck off")
+if user_choice not in ["rock","paper","scissor", "Rock", "Paper", "Scissor"]:
+    print("")
+    print ("Fuck Off")
+else:
+    print("")
+    print ("Selection: ", user_choice)
