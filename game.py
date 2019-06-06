67b38d523260d4ca8c69dda59d79893164a04dfe
esl405 <50837406+esl405@users.noreply.github.com>
2019-06-05 20:13:34 -0400

    Updated

--- game.py
+++ game.py
@@ -1,3 +1,5 @@
+import random
+
 print("Welcome to R-P-S-InClass")
 
 user_choice=input("Select: Rock, Paper, Scissors: ")
@@ -6,9 +8,29 @@ user_choice=input("Select: Rock, Paper, Scissors: ")
 #print ("Selection:")
 #else:
 #    print ("fuck off")
-if user_choice not in ["rock","paper","scissor", "Rock", "Paper", "Scissor"]:
+options = ["Rock", "Paper", "Scissor"]
+
+if user_choice not in options:
     print("")
     print ("Fuck Off")
 else:
     print("")
     print ("Selection: ", user_choice)
+
+Comp_choice = random.choice(options)
+print("Comp_choice: ", Comp_choice)
+
+if user_choice == Comp_choice:
+    print ("Tie")
+elif user_choice == "Rock" and Comp_choice == "Paper":
+    print (user_choice)
+elif user_choice == "Rock" and Comp_choice == "Scissor":
+    print (Comp_choice)
+elif user_choice == "Paper" and Comp_choice== "Rock":
+    print (user_choice)
+elif user_choice == "Paper" and Comp_choice == "Scissor":
+    print (Comp_choice)
+elif user_choice == "Scissor" and Comp_choice== "Paper":
+    print (user_choice)
+elif user_choice == "Scissor" and Comp_choice == "Rock":
+    print (Comp_choice)
