0692faaa28c3dc267f2593796510200dc8ea7339
esl405 <50837406+esl405@users.noreply.github.com>
2019-06-05 20:46:20 -0400

    updated with test

--- game.py
+++ game.py
@@ -1,36 +1,46 @@
 import random
 
-print("Welcome to R-P-S-InClass")
+def my_message():
+    return "HELLO"
 
-user_choice=input("Select: Rock, Paper, Scissors: ")
-
-#if user_choice in ["rock", "paper","scissors"]:
-#print ("Selection:")
-#else:
-#    print ("fuck off")
-options = ["Rock", "Paper", "Scissor"]
-
-if user_choice not in options:
-    print("")
-    print ("Fuck Off")
-else:
-    print("")
-    print ("Selection: ", user_choice)
-
-Comp_choice = random.choice(options)
-print("Comp_choice: ", Comp_choice)
-
-if user_choice == Comp_choice:
-    print ("Tie")
-elif user_choice == "Rock" and Comp_choice == "Paper":
-    print (user_choice)
-elif user_choice == "Rock" and Comp_choice == "Scissor":
-    print (Comp_choice)
-elif user_choice == "Paper" and Comp_choice== "Rock":
-    print (user_choice)
-elif user_choice == "Paper" and Comp_choice == "Scissor":
-    print (Comp_choice)
-elif user_choice == "Scissor" and Comp_choice== "Paper":
-    print (user_choice)
-elif user_choice == "Scissor" and Comp_choice == "Rock":
-    print (Comp_choice)
+#if this script is executed from command line
+if __name__ == "__main__":
+    pass
+    
+    print("Welcome to R-P-S-InClass")
+    
+    user_choice=input("Select: Rock, Paper, Scissors: ")
+    
+    #if user_choice in ["rock", "paper","scissors"]:
+    #print ("Selection:")
+    #else:
+    #    print ("fuck off")
+    options = ["Rock", "Paper", "Scissor"]
+    
+    if user_choice not in options:
+        print("")
+        print ("Faheem Says 'dumbass!'")
+    else:
+        print("")
+        print ("Selection: ", user_choice)
+    
+    Comp_choice = random.choice(options)
+    print("Comp_choice: ", Comp_choice)
+    
+    if user_choice == Comp_choice:
+        print ("Tie")
+    elif user_choice == "Rock" and Comp_choice == "Paper":
+        print (user_choice)
+    elif user_choice == "Rock" and Comp_choice == "Scissor":
+        print (Comp_choice)
+    elif user_choice == "Paper" and Comp_choice== "Rock":
+        print (user_choice)
+    elif user_choice == "Paper" and Comp_choice == "Scissor":
+        print (Comp_choice)
+    elif user_choice == "Scissor" and Comp_choice== "Paper":
+        print (user_choice)
+    elif user_choice == "Scissor" and Comp_choice == "Rock":
+        print (Comp_choice)
+    
+    #breakpoint
+    
---
+++ game_test.py
@@ -0,0 +1,8 @@
+from game import my_message #import from a file the fxn my_message
+
+def test(): #FXN name needs to start with "test"
+    print("Screw!")
+def test_my_message():
+    x = my_message()
+    assert x == "HELLO"
+    
